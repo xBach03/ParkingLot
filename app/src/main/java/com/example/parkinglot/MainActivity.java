d15317751929fcc072a4b49d1483a87870f10f1b
xBach <dangxuanbach03@gmail.com>
2024-03-13 23:00:02 +0700

    Added navigation bar

--- app/src/main/java/com/example/parkinglot/MainActivity.java
+++ app/src/main/java/com/example/parkinglot/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.parkinglot;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mainBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        mainBtn = findViewById(R.id.mainBtn);
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_login);
+            }
         });
     }
 }
