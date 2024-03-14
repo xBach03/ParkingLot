a2f176285064167e9dd40d602c3f366a1c9e9972
xBach <dangxuanbach03@gmail.com>
2024-03-14 22:56:50 +0700

    Set onclick for bottom navigation bar

--- app/src/main/java/com/example/parkinglot/MainActivity.java
+++ app/src/main/java/com/example/parkinglot/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView Navigator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +25,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_view, StatisticsFragment.class, null)
-                    .commit();
-        }
+        Navigator = findViewById(R.id.btmNavigation);
+        LoadFragment(new StatisticsFragment());
+        Navigator.setOnItemSelectedListener(menuItem -> {
+            int id = menuItem.getItemId();
+            if(id == R.id.historyManager){
+                LoadFragment(new ManagerFragment());
+            } else if(id == R.id.reservation){
+                LoadFragment(new ReservationFragment());
+            } else if(id == R.id.payment){
+                LoadFragment(new PaymentFragment());
+            } else {
+                LoadFragment(new StatisticsFragment());
+            }
+            return true;
+        });
+    }
+    private void LoadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        transaction.replace(R.id.fragment_container_view, fragment, null);
+        transaction.commit();
     }
 }
